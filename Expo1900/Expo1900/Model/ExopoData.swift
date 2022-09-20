a308671f2a3de3e76311b0e488b5a82bdeba8c26
wormsJJG <jjr66461837@gmail.com>
2022-09-20 14:11:33 +0900

    feat: 코딩키 선언

--- Expo1900/Expo1900/Model/ExopoData.swift
+++ Expo1900/Expo1900/Model/ExopoData.swift
@@ -8,9 +8,16 @@
 import Foundation
 
 struct ExpoData: Codable {
-    let name: String
-    let imageName: String
-    let shortDesc: String
-    let desc: String
+    var name: String
+    var imageName: String
+    var shortDesc: String
+    var desc: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDesc = "short_desc"
+        case desc
+    }
 }
 
