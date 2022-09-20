1a9d0c7116cb64f40af6e218f494f34ed5225c6e
wormsJJG <jjr66461837@gmail.com>
2022-09-20 13:46:03 +0900

    feat: ExpoData 구조체 생성

---
+++ Expo1900/Expo1900/Model/ExopoData.swift
@@ -0,0 +1,16 @@
+//
+//  ExopoData.swift
+//  Expo1900
+//
+//  Created by 정재근 on 2022/09/20.
+//
+
+import Foundation
+
+struct ExpoData: Codable {
+    let name: String
+    let imageName: String
+    let shortDesc: String
+    let desc: String
+}
+
