fbec60baaf3d5f036f5907e931c634269b884fbd
lcy <pgbenny0035@gmail.com>
2022-09-21 16:17:16 +0900

    refector:  구조체 프로퍼티 네이밍 변경

--- Expo1900/Expo1900/Model/EntryResponse.swift
+++ Expo1900/Expo1900/Model/EntryResponse.swift
@@ -18,13 +18,13 @@ struct Exhibition: Decodable {
 struct WorkPieceInformation: Decodable {
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
     
     enum CodingKeys: String, CodingKey {
         case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
-        case desc
+        case shortDescription = "short_desc"
+        case description
     }
 }
