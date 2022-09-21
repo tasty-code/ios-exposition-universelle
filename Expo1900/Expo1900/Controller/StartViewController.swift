7507d0d882655b1859e7399967b52f19bf8d3319
moonsoungjin <seongjinmun74@gmail.com>
2022-09-21 16:15:31 +0900

    feat: exposition decode

--- Expo1900/Expo1900/Controller/StartViewController.swift
+++ Expo1900/Expo1900/Controller/StartViewController.swift
@@ -7,10 +7,27 @@
 import UIKit
 
 class StartViewController: UIViewController {
+    var exposition: Exposition?
+    
+    private func getExposition() throws {
+        guard let jsonData: NSDataAsset = NSDataAsset(name: "exposition") else {
+            throw ExpoError.NilPersingData
+        }
+        do {
+            exposition = try PerserJSON().persingExpositionJSON(jsonData)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        do {
+            try getExposition()
+        } catch {
+            print(error.localizedDescription)
+        }
         
     }
+    
 }
-
