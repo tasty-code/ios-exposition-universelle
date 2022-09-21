1041e883d799171e7ac65bc56f0b606ab4b446c3
seunghui5247 <seunghui5247@gmail.com>
2022-09-21 10:02:00 +0900

    feat: 출품작 데이터 JSON모델 추가

--- Expo1900/Expo1900/Model/JSONModel.swift
+++ Expo1900/Expo1900/Model/JSONModel.swift
@@ -14,3 +14,16 @@ struct ExpoDesc: Decodable {
     let duration: String
     let description: String
 }
+
+struct ExpositionData: Decodable {
+    let name: String
+    let imageName: String
+    let shortDesc: String
+    let desc: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, desc
+        case imageName = "image_name"
+        case shortDesc = "short_desc"
+    }
+}
