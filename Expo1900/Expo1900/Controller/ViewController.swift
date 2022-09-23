b4a10985519ba354b9da2ed8f647af54651e42bb
이은호 <ieunho@ieunhoui-MacBookAir.local>
2022-09-23 11:50:17 +0900

    STEP2 브랜치 생성

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -8,6 +8,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var exhibitionData: Exhibition?
+    
+    func jsonParsingData() {
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        let jsonDecoder = JSONDecoder()
+        exhibitionData = try? jsonDecoder.decode(Exhibition.self, from: dataAsset.data)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
