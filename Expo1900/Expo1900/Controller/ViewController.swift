63cc308596a00b25e1609ed4669050ed9b0e4635
Schro2109 <bloodparty297@gmail.com>
2022-09-23 15:21:58 +0900

    STEP1 상태로 초기화

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -7,17 +7,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var expositionData: Exposition?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func getExpositionParsingData() {
-        guard let expositionDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
-            return
-        }
-        let expositionJsonDecoder = JSONDecoder()
-        
-        expositionData = try? expositionJsonDecoder.decode(Exposition.self, from: expositionDataAsset.data)
-    }
 }
