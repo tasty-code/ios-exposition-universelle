1b7968ad9621540e0418dcce0cc637b0658d62f9
moonsoungjin <seongjinmun74@gmail.com>
2022-09-23 16:14:01 +0900

    feat: DescriptionView 컴포넌트 생성, 오토레이웃

---
+++ Expo1900/Expo1900/Controller/DescriptionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DescriptionViewController.swift
+//  Expo1900
+//
+//  Created by 문성진 on 2022/09/23.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+    var workName: String = ""
+    var workImageName: String = ""
+    var workDescription: String = ""
+    
+    @IBOutlet weak private var workImageView: UIImageView!
+    @IBOutlet weak private var workLabel: UILabel!
+    
+    private func settingViewController() {
+        self.navigationController?.navigationItem.title = workName
+        workImageView.image = UIImage(named: workImageName)
+        workLabel.text = workDescription
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingViewController()
+    }
+}
