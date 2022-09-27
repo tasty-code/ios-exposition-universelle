03185c4357ec9ee4e99fb876623409b53f9e09e5
seunghui5247 <seunghui5247@gmail.com>
2022-09-27 15:31:37 +0900

    feat: 두번째 화면 추가

---
+++ Expo1900/Expo1900/Controller/ExpoItemViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ExpoItemViewController.swift
+//  Expo1900
+//
+//  Created by 천승희 on 2022/09/27.
+//
+
+import UIKit
+
+class ExpoItemViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
