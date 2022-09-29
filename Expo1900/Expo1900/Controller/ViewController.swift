6d66b91ac4a48c793d0a5735be1169ae3301e12e
Schro2109 <bloodparty297@gmail.com>
2022-09-29 10:38:22 +0900

    feat: expositionData를 이용한 ui 데이터 세팅 함수 구현

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -6,27 +6,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var expositionData: Exposition?
+class ExpositionViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        expositionData = try? expositionParsingData()
+        
+        guard let expositionData = try? expositionParsingData() else {
+            return
+        }
+        
+        setUiData(from: expositionData)
     }
     
     func expositionParsingData() throws -> Exposition? {
         guard let expositionDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
             throw ExpositionError.parsingError
         }
+        
         let expositionJsonDecoder = JSONDecoder()
-        
         let parsingExpositionData = try? expositionJsonDecoder.decode(Exposition.self, from: expositionDataAsset.data)
-        
         return parsingExpositionData
     }
     
-    func setUiData() {
-        
+    func setUiData(from data: Exposition) {
+        titleLabel.text = data.title
+        visitorsLabel.text = "방문객 : \(numberFormatToDecimal(of: data.visitors)) 명"
+        locationLabel.text = "개최지 : \(data.location)"
+        durationLabel.text = "개최 기간 : \(data.duration)"
+        descriptionLabel.text = data.description
     }
     
+    func numberFormatToDecimal(of number: Int?) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        guard let newNumber = number, let result = numberFormatter.string(from: NSNumber(value: newNumber)) else {
+            return "NaN"
+        }
+        
+        return result
+    }
 }
