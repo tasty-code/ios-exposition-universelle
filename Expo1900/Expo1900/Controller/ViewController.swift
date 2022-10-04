ca0aee63c9598a8fccdb5363e07ec655a5739824
이은호 <ieunho@ieunhoui-MacBookAir.local>
2022-10-04 14:15:38 +0900

    feat: setLabel함수 구현

--- Expo1900/Expo1900/Controller/ViewController.swift
+++ Expo1900/Expo1900/Controller/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController {
     var exhibitionData: Exhibition?
     var workPieceInformationData = [WorkPieceInformation]()
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     func exhibitionDataJsonParsingData() {
         guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
         let jsonDecoder = JSONDecoder()
@@ -23,12 +30,30 @@ class ViewController: UIViewController {
         workPieceInformationData = try! jsonDecoder.decode([WorkPieceInformation].self, from: dataAsset.data)
     }
     
+    func setLabel() {
+        guard let exhibitionData = exhibitionData else {
+            return
+        }
+        titleLabel.text = exhibitionData.title
+        visitorLabel.text = "\(exhibitionData.visitors)"
+        locationLabel.text = exhibitionData.location
+        durationLabel.text = exhibitionData.duration
+        descriptionLabel.text = exhibitionData.description
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         exhibitionDataJsonParsingData()
         workPieceInformationJsonParsingData()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.backButtonTitle = "메인"
+        setLabel()
+        
+        
+        
+        
     }
+//    print(workPieceInformationData[0].name)
 
 }
 
